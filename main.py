a279931627bd38707759d06ae8df62839e94ef5b
tusharharyana <123148711+tusharharyana@users.noreply.github.com>
2024-09-19 09:55:53 +0530

    Task Sprinter setup

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f75dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+#UI 
+window = Tk()
+window.title("Task Sprinter")
+
+window.mainloop()
